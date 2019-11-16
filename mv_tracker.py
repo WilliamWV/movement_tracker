6a05ce8e85fb23a4dc2e530df03a0033113aa649
WilliamWV <william.w.vargas@gmail.com>
2019-11-16 15:35:52 -0300

    Avoid rectangle superposition

--- mv_tracker.py
+++ mv_tracker.py
@@ -21,7 +21,9 @@ sigma_space = 50
 min_area = 1000
 # Luminance Value used when converting image to HSV and normalizing V
 # to avoid shaddow detection
-HSV_default_value = 150
+HSV_default_value = 100
+# Minimum distance to two detected bounding boxes to be considered differents
+min_rec_dist = 10
 
 ### KEYS ###
 pause_key = 'p'
@@ -42,25 +44,87 @@ def frame_normalization(frame):
 
     frame = imutils.resize(frame, width=default_width)
 
-    prev_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    thresh_t = cv2.threshold(prev_gray, 20, 1, cv2.THRESH_BINARY)[1]
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #thresh_t = cv2.threshold(prev_gray, 20, 1, cv2.THRESH_BINARY)[1]
 
-    frame[:,:,0] = np.multiply(frame[:,:,0], thresh_t)
-    frame[:,:,1] = np.multiply(frame[:,:,1], thresh_t)
-    frame[:,:,2] = np.multiply(frame[:,:,2], thresh_t)
+    #frame[:,:,0] = np.multiply(frame[:,:,0], thresh_t)
+    #frame[:,:,1] = np.multiply(frame[:,:,1], thresh_t)
+    #frame[:,:,2] = np.multiply(frame[:,:,2], thresh_t)
 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 
-    hsv[:,:,2] = np.multiply(HSV_default_value, thresh_t)
-    hsv = cv2.GaussianBlur(hsv, (7, 7), 0)
-    cv2.imshow("HSV", cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR))
+    #hsv[:,:,2] = np.multiply(HSV_default_value, thresh_t)
+    #hsv = cv2.GaussianBlur(hsv, (7, 7), 0)
+    #cv2.imshow("HSV", cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR))
 
-    gray_frame = cv2.cvtColor(cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2GRAY)
-    gray_frame = cv2.bilateralFilter(gray_frame, neighborhood_size, \
-        sigma_color, sigma_space)
+    #gray_frame = cv2.cvtColor(cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2GRAY)
+    #gray_frame = cv2.bilateralFilter(gray_frame, neighborhood_size, \
+    #    sigma_color, sigma_space)
 
     return frame, gray_frame
+
+
+def superposition(rec1, rec2):
+    (x1, y1, w1, h1) = rec1
+    (x2, y2, w2, h2) = rec2
+
+    return (x1 < x2 and x1 + w1 > x2 and y1 < y2 and y1 + h1 > y2) or \
+        (x1 < x2 and x1 + w1 > x2 and y1 > y2 and y1 < y2 + h2) or \
+        (x1 > x2 and x1 < x2 + w2 and y1 < y2 and y1 + h1 > y2) or \
+        (x1 > x2 and x1 < x2 + w2 and y1 > y2 and y1 < y2 + h2)
+
+
+def euclidian_distance(p1, p2):
+    (x1, y1) = p1
+    (x2, y2) = p2
+    return sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+def rec_distance(rec1, rec2):
+    (x1, y1, w1, h1) = rec1
+    (x2, y2, w2, h2) = rec2
+
+    if superposition(rec1, rec2):
+        return 0
+    else:
+        c1 = (x1 + w1/2, y1 + h1/2)
+        c2 = (x2 + w2/2, y2 + h2/2)
+        dx = abs(c1[0] - c2[0]) - w1/2 - w2/2
+        dy = abs(c1[1] - c2[1]) - h1/2 - h2/2
+
+        return max(dx, dy)
+
+def merge_rects(rec1, rec2):
+    (x1, y1, w1, h1) = rec1
+    (x2, y2, w2, h2) = rec2
+    x = min(x1, x2)
+    y = min(y1, y2)
+    w = max(x1 + w1, x2 + w2) - x
+    h = max(y1 + h1, y2 + h2) - y
+    return (x, y, w, h)
+
+
+# Avoid superpositions and rectangles too close
+# This code will check with all pairs of rectangles if they are too close
+# When a pair that is too close is found the rectangles are merged and
+# a recursive call is made with a reduced list of rectangles removing the previous
+# pair and adding the merged result
+
+def adjust_rectangles(rectangles):
+    final_rects = []
+    modifieds = []
+    for i in range(len(rectangles)):
+        for j in range(i+1, len(rectangles)):
+            r0 = rectangles[i]
+            r1 = rectangles[j]
+            if rec_distance(r0, r1) < min_rec_dist:
+                rec = merge_rects(r0, r1)
+                rectangles.remove(r0)
+                rectangles.remove(r1)
+                rectangles.append(rec)
+                return adjust_rectangles(rectangles)
+
+    return rectangles
 
 
 def process_frame(frame, base_frame):
@@ -69,20 +133,28 @@ def process_frame(frame, base_frame):
     frame_diff = cv2.absdiff(base_frame, gray_frame)
     ret, binary_frame = cv2.threshold (frame_diff, diff_threshold, 255, cv2.THRESH_BINARY)
 
+    binary_frame = cv2.erode(binary_frame, None, iterations = 1)
     binary_frame = cv2.dilate(binary_frame, None, iterations = 1)
     contours = cv2.findContours(binary_frame.copy(), cv2.RETR_EXTERNAL,\
         cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
 
+    rectangles = []
     for c in contours:
         if cv2.contourArea(c) < min_area:
             continue
 
         (x, y, w, h) = cv2.boundingRect(c)
+        rectangles.append((x, y, w, h))
+
+    rectangles = adjust_rectangles(rectangles)
+
+    for rectangle in rectangles:
+        (x, y, w, h) = rectangle
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
 
     cv2.imshow('Camera', frame)
-    cv2.imshow('Movements', binary_frame)
+    cv2.imshow('Binary frame', binary_frame)
     cv2.imshow('Frame diff', frame_diff)
     cv2.imshow('Gray frame', gray_frame)
 
@@ -114,15 +186,17 @@ def process_pause(video, base_frame, current_frame_index):
 
 
 def track_movements(video):
-
     base_frame = None
     current_frame_index = 0
 
     while True:
         valid_frame, frame = video.read()
+        cv2.waitKey(0)
+
         if not valid_frame or frame is None:
             # If the frame is not valid then the video ended
             break
+
 
         if base_frame is not None:
             process_frame(frame, base_frame)
